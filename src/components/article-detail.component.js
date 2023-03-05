13a9e9c9adba753ac23417e4729e082e6c2e90ad
Mücahit Çoban <muco@muco>
2023-03-06 01:14:09 +0300

    image added

--- src/components/article-detail.component.js
+++ src/components/article-detail.component.js
@@ -3,6 +3,8 @@ const ArticleDetail = () => (
         <div className="row">
             <div className="col-12 pt-3 text-start">
                 <h5 className="card-title">Article Tile</h5>
+                <img className="w-50 mb-3 mt-3" src="https://bitnetinfotech.com/wp-content/uploads/2022/08/Frameworks-for-React-JS.jpg" alt="Card image cap" />
+
                 <h6>17.17.2018</h6>
                 <p className="card-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
